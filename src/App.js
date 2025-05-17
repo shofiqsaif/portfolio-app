b313291145a320ec50155bf9da4ea5ccf569dd44
Shofiq Saif <sislam161079@gmail.com>
2025-05-17 20:03:15 +0600

    import react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import WorkExperience from "./components/WorkExperience";
