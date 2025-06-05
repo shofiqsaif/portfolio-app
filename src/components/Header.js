4633b58ea005cc27c74d93ae824121c83dcf1e26
Shofiq Saif <sislam161079@gmail.com>
2025-06-06 02:08:52 +0600

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,38 @@ import "./Header.css"; // We'll create this CSS file next
 function Header() {
   return (
     <header className="header">
-      <nav>
-        <ul>
-          {/* <li>
-            <a href="#about">About</a>
-          </li> */}
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#education">Education</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          {/* <li>
-            <a href="#achievements">Achievements</a>
-          </li> */}
-          <li>
-            <a href="#skills">Skills</a>
-          </li>
+      <div className="header-content">
+        <div className="logo">
+          {/* Replace with your logo image if available */}
+          <a href="#">
+            <img src="/logo192.png" alt="Logo" />{" "}
+          </a>
+        </div>
+        <nav>
+          <ul>
+            {/* <li>
+              <a href="#about">About</a>
+            </li> */}
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            {/* <li>
+              <a href="#achievements">Achievements</a>
+            </li> */}
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
 
-          {/* Add more navigation links as needed */}
-        </ul>
-      </nav>
+            {/* Add more navigation links as needed */}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
