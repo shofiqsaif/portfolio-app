f5e45fbec37393b2d12e6ba6abe5f7fe7623007f
Shofiq Saif <sislam161079@gmail.com>
2025-06-06 02:11:13 +0600

    Revert "A Logo implemented"
    This reverts commit 48980c4986ec4e00ff1b8f0a0358de993a868d2d.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,42 +4,30 @@ import "./Header.css"; // We'll create this CSS file next
 function Header() {
   return (
     <header className="header">
-      <div className="logo">
-        <a href="#about" className="logo-link">
-          <span
-            className="logo-text"
-            style={{ fontSize: "1.5rem", fontWeight: "bold", color: "white" }}
-          >
-            Shofiq
-          </span>
-        </a>
-      </div>
-      <div className="nav-wrapper">
-        <nav>
-          <ul>
-            {/* <li>
-              <a href="#about">About</a>
-            </li> */}
-            <li>
-              <a href="#experience">Experience</a>
-            </li>
-            <li>
-              <a href="#education">Education</a>
-            </li>
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            {/* <li>
-              <a href="#achievements">Achievements</a>
-            </li> */}
-            <li>
-              <a href="#skills">Skills</a>
-            </li>
+      <nav>
+        <ul>
+          {/* <li>
+            <a href="#about">About</a>
+          </li> */}
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#education">Education</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          {/* <li>
+            <a href="#achievements">Achievements</a>
+          </li> */}
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
 
-            {/* Add more navigation links as needed */}
-          </ul>
-        </nav>
-      </div>
+          {/* Add more navigation links as needed */}
+        </ul>
+      </nav>
     </header>
   );
 }
